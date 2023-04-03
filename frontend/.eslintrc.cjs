40557e5f806a75cdb6b9a03acf52679c726bf344
mantra-tech <99634633+mantra-tech@users.noreply.github.com>
2023-04-03 20:55:46 +0530

    Update .eslintrc.cjs

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -9,8 +9,16 @@ module.exports = {
         parser: '@typescript-eslint/parser',
     },
     extends: ['@nuxtjs/eslint-config-typescript', 'plugin:prettier/recommended'],
-    plugins: [],
+    plugins: ['prefer-arrow'],
     rules: {
+        'prefer-arrow/prefer-arrow-functions': [
+            'error',
+            {
+                disallowPrototype: true,
+                singleReturnOnly: false,
+                classPropertiesAllowed: false,
+            },
+        ],
         'no-console': ['warn', { allow: ['error'] }],
         'capitalized-comments': [
             'error',
